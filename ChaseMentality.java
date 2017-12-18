af99b43224b14a95f623d4897e62ee92a8087a1f
JermayneN <gkelectronics139@gmail.com>
2017-12-18 14:45:50 -0500

    analysis added

--- ChaseMentality.java
+++ ChaseMentality.java
@@ -27,27 +27,27 @@ public class ChaseMentality extends Controller<MOVE>
 		int minDistance=Integer.MAX_VALUE;
 		GHOST minGhost=null;		
 		
-		//go after the pills and power pills
+		
 		int[] pills=game.getPillIndices();
 		int[] powerPills=game.getPowerPillIndices();		
 		
 		ArrayList<Integer> targets=new ArrayList<Integer>();
 		
-		for(int i=0;i<pills.length;i++)					//finds the pills available			
+		for(int i=0;i<pills.length;i++)							
 			if(game.isPillStillAvailable(i))
 				targets.add(pills[i]);
 		
-		for(int i=0;i<powerPills.length;i++)			//finds the power pills available
+		for(int i=0;i<powerPills.length;i++)			
 			if(game.isPowerPillStillAvailable(i))
 				targets.add(powerPills[i]);				
 		
-		int[] targetsArray=new int[targets.size()];		//convert from ArrayList to array
+		int[] targetsArray=new int[targets.size()];		
 		
 		for(int i=0;i<targetsArray.length;i++)
 			targetsArray[i]=targets.get(i);
 		
 
-		//for chasing ghost when they blue 
+		//for chasing the ghost when they blue 
 		for(GHOST ghost : GHOST.values())
 			if(game.getGhostEdibleTime(ghost)>0)
 			{
@@ -62,11 +62,12 @@ public class ChaseMentality extends Controller<MOVE>
 		
 		
 		//return the next direction once the closest target has been identified
+
 		//if(minGhost!=null)
 			//return game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(),game.getClosestNodeIndexFromNodeIndex(current,targetsArray,DM.PATH),DM.PATH);
 			//didnt work as well
 		
-		if(minGhost!=null)	//we found an edible ghost
+		if(minGhost!=null)	
 			return game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(),game.getGhostCurrentNodeIndex(minGhost),DM.PATH);
 		 
 		return allMoves[rnd.nextInt(allMoves.length)]; 
